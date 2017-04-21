96aa6c98d4f72647b518a8c1f29d3d51088ef0d0
lisbi101 <lisbi101@mail.chapman.edu>
2017-04-20 18:25:38 -0700

    Finished the ClientListener

--- ClientListener.java
+++ ClientListener.java
@@ -1,12 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 
 public class ClientListener implements Runnable{
-    ClientListener(Socket socket) {
+    private Socket connectionSock = null;
 
+    ClientListener(Socket socket) {
+        this.connectionSock = socket;
     }
 
     public void run(){
-
+        try
+        {
+            BufferedReader serverInput = new BufferedReader(new InputStreamReader(connectionSock.getInputStream()));
+            while (true)
+            {
+                // Get data sent from the server
+                String serverText = serverInput.readLine();
+                if (serverInput != null)
+                {
+                    System.out.println(serverText);
+                    if(serverText.contains("Win") || serverText.contains("Lose"))
+                    {
+                        System.out.println("Ending Game");
+                        connectionSock.close();
+                        System.exit(0);
+                    }
+                }
+                else
+                {
+                    // Connection was lost
+                    System.out.println("Closing connection for socket " + connectionSock);
+                    connectionSock.close();
+                    break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error: " + e.toString());
+        }
     }
 }
