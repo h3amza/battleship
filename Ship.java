ad5b1aa255098a42175054fd25f6b592cc69fcfc
Rao Hamza Ali <ali121@mail.chapman.edu>
2017-04-30 22:23:12 -0700

    added comments

--- Ship.java
+++ Ship.java
@@ -1,3 +1,10 @@
+/*
+* This class defines a Ship that is placed on the board
+* Only useful as it is easier to implement as a separate class
+* based on the usage of orientation and future works with 
+* dynamic ship size
+*/
+
 public class Ship 
 {
 	public int length, x, y;
